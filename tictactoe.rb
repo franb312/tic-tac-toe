4249f406f959e742c54a71a6f1fa324b3a678538
franb312 <114260534+franb312@users.noreply.github.com>
2024-04-11 18:05:59 -0300

    Add files via upload

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Game
   def initialize
     @board = Array.new(10)
@@ -6,14 +8,21 @@ class Game
   attr_reader :board
 
   def print_board
-    col_separator, row_separator = "   |   ", "----+-------+----"
-    label_for_position = lambda{|position| @board[position] ? @board[position] : position}
-    
-    row_for_display = lambda{|row| row.map(&label_for_position).join(col_separator)}
-    row_positions = [[1,2,3], [4,5,6], [7,8,9]]
+    col_separator = '   |   '
+    row_separator = '----+-------+----'
+    label_for_position = ->(position) { @board[position] || position }
+
+    row_for_display = ->(row) { row.map(&label_for_position).join(col_separator) }
+    row_positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     rows_for_display = row_positions.map(&row_for_display)
-    puts rows_for_display.join("\n" + row_separator + "\n")
+    puts rows_for_display.join("\n#{row_separator}\n")
+  end
+
+  def picker(player_number)
+    chosen_number = gets.chomp.to_i
+    @board[chosen_number] = 0
   end
 end
 game = Game.new
+game.picker(1)
 game.print_board
